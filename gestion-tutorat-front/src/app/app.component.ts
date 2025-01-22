5fe768da672d21fdbb65b5741a5faa67cdf119a0
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2025-01-22 17:40:00 +0100

    set up du router

--- gestion-tutorat-front/src/app/app.component.ts
+++ gestion-tutorat-front/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ImportComponent } from './components/import/import.component';
-import { AfficheEtudiantsComponent } from "./affiche-etudiants/affiche-etudiants.component";
-import { TableComponent } from "./shared/table/table.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [ImportComponent, AfficheEtudiantsComponent, TableComponent],
+  imports: [ImportComponent, RouterModule],
 })
 export class AppComponent {
   title = 'gestion-tutorat-front';
--- gestion-tutorat-front/src/main.ts
+++ gestion-tutorat-front/src/main.ts
@@ -2,7 +2,21 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, Routes } from '@angular/router';
+import { AfficheEtudiantsComponent } from './app/affiche-etudiants/affiche-etudiants.component';
+import { ImportComponent } from './app/components/import/import.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent }, 
+  { path: 'etudiants/all', component: AfficheEtudiantsComponent },
+  { path: 'importer', component: ImportComponent },
+  { path: '**', redirectTo: '' },
+];
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()],
+  providers: [
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideRouter(routes),
+  ],
 }).catch((err) => console.error(err));
