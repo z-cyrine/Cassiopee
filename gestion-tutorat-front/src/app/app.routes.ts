c6ad247a81f7b37298e7978bf9b7144509f84094
sarraragguem <sarra.ragguem@insat.ucar.tn>
2025-05-11 21:10:37 +0200

    home

--- gestion-tutorat-front/src/app/app.routes.ts
+++ gestion-tutorat-front/src/app/app.routes.ts
@@ -1,19 +1,30 @@
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/etudiants/all', pathMatch: 'full' },
-  { path: 'etudiants/all', loadComponent: () => import('./components/affiche-etudiants/affiche-etudiants.component').then(m => m.AfficheEtudiantsComponent) },
+  // Page d'accueil (par défaut)
+  { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+
+  // Routes principales
   { path: 'import', loadComponent: () => import('./components/import/import.component').then(m => m.ImportComponent) },
+  { path: 'etudiants/all', loadComponent: () => import('./components/affiche-etudiants/affiche-etudiants.component').then(m => m.AfficheEtudiantsComponent) },
   { path: 'auto-affectation', loadComponent: () => import('./components/auto-affectation/auto-affectation.component').then(m => m.AutoAffectationComponent) },
+  { path: 'reporting', loadComponent: () => import('./reporting/reporting.component').then(m => m.ReportingComponent) },
+
+  // Créations
   { path: 'etudiant-create', loadComponent: () => import('./components/etudiant-create/etudiant-create.component').then(m => m.EtudiantCreateComponent) },
   { path: 'tuteur-create', loadComponent: () => import('./components/tuteur-create/tuteur-create.component').then(m => m.TuteurCreateComponent) },
   { path: 'majeure-create', loadComponent: () => import('./components/majeure-create/majeure-create.component').then(m => m.MajeureCreateComponent) },
-  { path: 'reporting', loadComponent: () => import('./reporting/reporting.component').then(m => m.ReportingComponent) },
+
+  // Détails
   { path: 'etudiants/:id', loadComponent: () => import('./components/etudiant-read/etudiant-read.component').then(m => m.EtudiantReadComponent) },
   { path: 'tuteurs/:id', loadComponent: () => import('./components/tuteur-read/tuteur-read.component').then(m => m.TuteurReadComponent) },
+  { path: 'majeures/:id', loadComponent: () => import('./components/majeure-read/majeure-read.component').then(m => m.MajeureReadComponent) },
+
+  // Éditions
   { path: 'etudiants/edit/:id', loadComponent: () => import('./components/etudiant-edit/etudiant-edit.component').then(m => m.EtudiantEditComponent) },
   { path: 'tuteurs/edit/:id', loadComponent: () => import('./components/tuteur-edit/tuteur-edit.component').then(m => m.TuteurEditComponent) },
-  { path: 'majeures/:id', loadComponent: () => import('./components/majeure-read/majeure-read.component').then(m => m.MajeureReadComponent) },
   { path: 'majeures/edit/:id', loadComponent: () => import('./components/majeure-edit/majeure-edit.component').then(m => m.MajeureEditComponent) },
-  { path: '**', redirectTo: '/etudiants/all' }
+
+  // Fallback
+  { path: '**', redirectTo: '' }
 ];
