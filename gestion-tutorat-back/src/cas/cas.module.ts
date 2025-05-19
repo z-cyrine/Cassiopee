4fe9bba7ca015676e0d3a24d7d4e86c61dc6da00
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2025-05-19 18:08:29 +0200

    removed auth

--- gestion-tutorat-back/src/cas/cas.module.ts
+++ gestion-tutorat-back/src/cas/cas.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { CasService } from './cas.service';
 import { CasController } from './cas.controller';
 import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
+
+import { JWT_SECRET } from 'src/modules/auth/config/jwt-secrets';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    HttpModule],
   controllers: [CasController],
   providers: [CasService],
 })
